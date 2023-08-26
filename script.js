36ef05050fb3eb6eb08b08f294a26246af1aacbe
ramus <ramussiz29@gmail.com>
2023-08-26 06:48:13 +0100

    update : TRANSLATE DATA

--- script.js
+++ script.js
@@ -26,19 +26,19 @@ function fetchFunction(urlSearch) {
                 resultat.innerHTML= `
                 <div class="block-flag">
                 <img src= ${data[0].flags.svg} class="img-flag" alt="flag" /> 
-                <h3> ${data[0].translations.fra.common}</h3>
+                <h3> ${data[0].name.common}</h3>
                 </div>
                 <div class="bloc-data">
                 <h3>Capital: <span>${data[0].capital[0]}</span></h3>
-                <h3>Continent: <span>${data[0].continents[0]}</span></h3>
+                <h3>Continents: <span>${data[0].continents[0]}</span></h3>
+                <h3>Languages <span>${Object.values(data[0].languages).toString().split(",").join(", ")}</span></h3>
+                <h3>Currencies: <span>${Object.keys(data[0].currencies)} ( ${data[0].currencies[Object.keys(data[0].currencies)].name} )</span></h3>
                 <h3>Population: <span>${data[0].population}</span></h3>
-                <h3>Monaie: <span>${Object.keys(data[0].currencies)} ( ${data[0].currencies[Object.keys(data[0].currencies)].name} )</span></h3>
-                <h3>Superficie: <span>${data[0].area}</span></h3>
-
-                <h3>Indicatif Tel: <span>${data[0].idd.root}${data[0].idd.suffixes[0]}</span></h3>
+                <h3>Area: <span>${data[0].area}</span></h3>
+                <h3>IDD: <span>${data[0].idd.root}${data[0].idd.suffixes[0]}</span></h3>
 
 
-                <h3>Langues parlées: <span>${Object.values(data[0].languages).toString().split(",").join(", ")}</span></h3>
+               
 
                 </div>
 
